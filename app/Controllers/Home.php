c42b878d65acb329f805d3060715d1bd02f21fc6
robinNcode <msmrobin518@gmail.com>
2021-09-13 01:24:14 +0600

    login checked incomplete

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,12 +2,24 @@
 
 namespace App\Controllers;
 
+use App\Models\LoginModel;
+
 /**
  * Class Home
  * @package App\Controllers
  */
 class Home extends BaseController
 {
+    protected $login = null;
+
+    /**
+     * Home constructor.
+     */
+    public function __construct()
+    {
+        $this->login = new LoginModel();
+    }
+
     public function index()
     {
         return view('Views/Auth/Login');
@@ -15,7 +27,12 @@ class Home extends BaseController
 
     public function loginAttempt()
     {
-        dd($this->request->getPost());
+        $user = $this->request->getPost('user_name', FILTER_SANITIZE_STRING);
+        $password = $this->request->getPost('password', FILTER_SANITIZE_STRING);
+
+        if(!empty($user) && !empty($password)){
+
+        }
     }
 
     public function create()
